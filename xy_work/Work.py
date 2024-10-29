cf04a8f87ec6372d0ede617f7ef45201ad999cec
yuyanget <845262968@qq.com>
2024-10-29 21:09:15 +0800

    release 1.0.14

--- xy_work/Work.py
+++ xy_work/Work.py
@@ -63,6 +63,7 @@ class Work(ArgParse):
                 -----project => 项目
                 -----runner  => 运行启动器
             """,
+            nargs="+",
         )
         self.add_argument(
             flag="-n",
@@ -70,6 +71,7 @@ class Work(ArgParse):
             help_text="""
                 项目名称 仅支持英文(当[command=project])
             """,
+            nargs="+",
         )
 
     def on_arguments(
